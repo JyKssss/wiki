2cfe7fda7c744acd47619c70d75dbaf6f0e6d627
Junyuan <junyuantanhere@gmail.com>
2021-03-26 01:41:48 +0800

    2-8 增添自定义配置项

--- src/main/java/com/junyuan/wiki/controller/TestController.java
+++ src/main/java/com/junyuan/wiki/controller/TestController.java
@@ -1,21 +1,25 @@
 package com.junyuan.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class TestController {
 
+    /*
+    :后是默认值
+     */
+    @Value("${test.hello:TEST}")
+    private String testHello;
     /*
     Get Post Put Delete
     RequestMapping 注解支持所有http访问方式
      */
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
-        return "Hello World";
+        return "Hello World " + testHello;
     }
 
     @RequestMapping(value = "/hello/post", method = RequestMethod.POST)
