c444246a5a2fc761e4cb12678106f212fc89cc64
Junyuan <junyuantanhere@gmail.com>
2021-03-27 04:14:48 +0800

    3-5 集成mybatis-generator 生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,14 @@ create table test(
     password varchar(50) comment '密码',
     primary key (id)
 )engine=innodb default charset =utf8mb4 comment '测试'
+
+
+drop table if exists demo;
+create table demo(
+                     id  int(15) not null comment 'id',
+                     name varchar(50) comment '名称',
+                     primary key (id)
+)engine=innodb default charset =utf8mb4 comment '测试'
+
+
+insert into demo (id, name) values (1,'test')
