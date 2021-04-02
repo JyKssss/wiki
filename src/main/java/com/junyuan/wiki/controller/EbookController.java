5389354389c872ff35b49d827331f379130a0940
Junyuan <junyuantanhere@gmail.com>
2021-04-02 16:08:47 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/junyuan/wiki/controller/EbookController.java
+++ src/main/java/com/junyuan/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.junyuan.wiki.controller;
 
-import com.junyuan.wiki.domain.Ebook;
+import com.junyuan.wiki.req.EbookReq;
 import com.junyuan.wiki.resp.CommonResp;
+import com.junyuan.wiki.resp.EbookResp;
 import com.junyuan.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +19,12 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
 
-
-
+    //自动映射到req中的相应属性
+    //controller层不要出现domain实体
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp= new CommonResp<>();
-        List<Ebook> list=ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp= new CommonResp<>();
+        List<EbookResp> list=ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
