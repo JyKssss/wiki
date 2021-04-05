a2dbfc395250f2a188f2939364fef959cc56268e
Junyuan <junyuantanhere@gmail.com>
2021-04-05 23:50:03 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Junyuan Tan Wiki ©2021
+  </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
