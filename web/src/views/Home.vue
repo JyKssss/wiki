7004ad7a990f32db00c60a3f98f944a975bfa69e
Junyuan <junyuantanhere@gmail.com>
2021-04-12 00:14:26 +0800

    5-5 配置axios拦截器 打印请求和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -94,17 +94,17 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup(){
-    console.log("setup");
+    // console.log("setup");
     const ebooks=ref();
     const ebooks1=reactive({books:[]});
 
     onMounted(() =>{
-      console.log("onMounted");
+      // console.log("onMounted");
       axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then(function (response) {
         const data= response.data;
         ebooks.value=data.content;
         ebooks1.books=data.content;
-        console.log(response);
+        // console.log(response);
       });
     })
 
