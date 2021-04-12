872a6746dcada07eaa446939b65a4b9bd9b982d3
Junyuan <junyuantanhere@gmail.com>
2021-04-13 02:22:39 +0800

    5-6 增加AOP 打印接口耗时 请求参数和返回参数 去掉了过滤器和拦截器

--- src/main/java/com/junyuan/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/junyuan/wiki/config/SpringMvcConfig.java
@@ -1,24 +1,20 @@
 package com.junyuan.wiki.config;
 
-import com.junyuan.wiki.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    LogInterceptor logInterceptor;
-
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-
-
-
-    }
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//
+//
+//
+//    }
 }
