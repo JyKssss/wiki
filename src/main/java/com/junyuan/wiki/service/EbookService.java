633bde5bc2fab19a3e06027563e85a5216b008c2
Junyuan <junyuantanhere@gmail.com>
2021-04-19 03:37:18 +0800

    6-4 使用PageHelper实现后端分页 生成总页数 总条数参数

--- src/main/java/com/junyuan/wiki/service/EbookService.java
+++ src/main/java/com/junyuan/wiki/service/EbookService.java
@@ -1,11 +1,16 @@
 package com.junyuan.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.junyuan.wiki.config.WikiApplication;
 import com.junyuan.wiki.domain.Ebook;
 import com.junyuan.wiki.domain.EbookExample;
 import com.junyuan.wiki.mapper.EbookMapper;
 import com.junyuan.wiki.req.EbookReq;
 import com.junyuan.wiki.resp.EbookResp;
 import com.junyuan.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +19,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -24,9 +31,12 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
-
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
+        PageInfo<Ebook> pageInfo=new PageInfo<>(ebookList);
+        LOG.info("总行数: {}",pageInfo.getTotal());
+        LOG.info("总页数: {}",pageInfo.getPages());
 //        List<EbookResp>respList=new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
