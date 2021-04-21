dca1befe26832ffd91d0ae2fc510ff3317bb045f
Junyuan <junyuantanhere@gmail.com>
2021-04-22 01:25:13 +0800

    6-12  编辑时复制对象 避免还未编辑完成就影响列表数据

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -100,6 +100,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
+import {Tool} from "@/util/tool";
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -225,7 +226,7 @@ export default defineComponent({
 
     const edit= (record: any) =>{
       modalVisible.value=true;
-      ebook.value=record;
+      ebook.value=Tool.copy(record);
     }
 
     /**
