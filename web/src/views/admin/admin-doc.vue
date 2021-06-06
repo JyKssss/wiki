3af0448e01bb878d676cad793e287ea79b98e02b
Junyuan <junyuantanhere@gmail.com>
2021-06-07 00:40:46 +0800

    8-11 增加保存文档成功提示 编辑和新增时清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -223,8 +223,8 @@ export default defineComponent({
         modalLoading.value = false;
         const data = response.data;
         if (data.success){
-          modalVisible.value = false;
-
+          // modalVisible.value = false;
+          message.success("保存成功");
           //重新加载列表数据
           handleQuery();
         }
@@ -305,6 +305,8 @@ export default defineComponent({
      */
 
     const edit= (record: any) =>{
+      //清空富文本框
+      editor.txt.html("");
       modalVisible.value=true;
       doc.value=Tool.copy(record);
       handleQueryContent()
@@ -321,6 +323,8 @@ export default defineComponent({
      */
 
     const add= () =>{
+      //清空富文本框
+      editor.txt.html("");
       modalVisible.value=true;
       doc.value= {
         ebookId: route.query.ebookId
